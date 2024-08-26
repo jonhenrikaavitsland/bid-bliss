9caa5ea2f81da43b6e895be18c51a51d49402031
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-26 02:16:14 +0200

    function to open any modal based on inputs

--- src/js/ui/modal/openModal.js
+++ src/js/ui/modal/openModal.js
@@ -1,6 +1,9 @@
+import { closeModal } from './closeModal';
+
 export function openModal(target) {
   const modal = document.querySelector(`[data-modal="modalObject"]`);
-  console.log(modal);
+
+  if (!target) closeModal();
 
   modal.classList.remove('hidden');
   modal.classList.add('flex', 'items-center', 'justify-center', 'px-5');
