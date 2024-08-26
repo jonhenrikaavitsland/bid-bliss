2e79f67b5aae5396ab8d165a000e0604664211dd
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-26 02:17:11 +0200

    this function will tell the run function what modal is to be shown

---
+++ src/js/router/modalRouter.js
@@ -0,0 +1,14 @@
+import { loginModal } from '../../template/loginModal.js';
+import { registerModal } from '../../template/registerModal.js';
+
+export function modalRouter(modalValue) {
+  const modalLogin = loginModal();
+  const modalRegister = registerModal();
+
+  switch (modalValue) {
+    case 'login':
+      return modalLogin;
+    case 'register':
+      return modalRegister;
+  }
+}
