9ad6ff2c21a4838a436f579fd8da5d89ced50ff7
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-26 02:17:43 +0200

    this function is the entry point to displaying a modal

---
+++ src/js/ui/modal/runModal.js
@@ -0,0 +1,10 @@
+import { modalRouter } from '../../router/modalRouter.js';
+import { openModal } from './openModal.js';
+
+export function runModal(state = 'false', modalValue) {
+  const currentModal = modalRouter(modalValue);
+
+  if (state === true) {
+    openModal(currentModal);
+  }
+}
