6991066b63dc8717a9c2de5bf93f0bb2acec8361
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-26 07:14:00 +0200

    function to format time to text following my specifications

---
+++ src/js/data/formatDateTime.js
@@ -0,0 +1,17 @@
+export function formatDateTime(datetimeString) {
+  const date = new Date(datetimeString);
+
+  if (isNaN(date)) return 'invalid date';
+
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZone: 'Europe/Oslo',
+    timeZoneName: 'short',
+  };
+
+  return date.toLocaleString('nb-NO', options);
+}
