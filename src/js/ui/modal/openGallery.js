cc174ae176547db29e30e9eec83064469a12a819
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-27 15:47:32 +0200

    function to open the gallery modal

---
+++ src/js/ui/modal/openGallery.js
@@ -0,0 +1,21 @@
+import { gallery } from '../../data/constants';
+import { closeGallery } from './closeGallery';
+
+export function openGallery(target) {
+  console.log(target);
+  if (!target) closeGallery(gallery);
+  const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+  gallery.classList.remove('hidden');
+  gallery.classList.add('flex', 'items-center', 'justify-center', 'px-5');
+  gallery.style.top = `${scrollPosition}px`;
+  gallery.append(target);
+
+  gallery.addEventListener('click', (event) => {
+    if (event.target === gallery) {
+      closeGallery(gallery);
+    } else {
+      return;
+    }
+  });
+}
