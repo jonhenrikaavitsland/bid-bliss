bcef5660a73f0b7563f0095143eacc0a66db6568
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-27 15:50:10 +0200

    gallery modal

---
+++ src/js/template/galleryModal.js
@@ -0,0 +1,30 @@
+import { createDiv } from '../elements/createDiv';
+import { createImg } from '../elements/createImg';
+import { load } from '../localStorage/load';
+
+const DEFAULT_IMAGE_URL = '/src/images/placeholderItem.png';
+const DEFAULT_IMAGE_ALT = 'listing item';
+
+export function galleryModal() {
+  const media = load('media');
+
+  const galleryContainer = createDiv('flex', 'flex-col', 'gap-10');
+
+  const activeImage = createImg(media[0].url || DEFAULT_IMAGE_URL, media[0].alt || DEFAULT_IMAGE_ALT, 'rounded-xl', 'aspect-square', 'object-contain', 'object-bottom');
+
+  const allImages = createDiv('flex', 'justify-between');
+
+  media.forEach((image) => {
+    const imageObject = createImg(image.url || DEFAULT_IMAGE_URL, image.alt || DEFAULT_IMAGE_ALT, 'rounded-xl', 'cursor-pointer', 'max-w-24');
+    imageObject.addEventListener('click', () => {
+      activeImage.src = image.url || DEFAULT_IMAGE_URL;
+      activeImage.alt = image.alt || DEFAULT_IMAGE_ALT;
+    });
+
+    allImages.append(imageObject);
+  });
+
+  galleryContainer.append(activeImage, allImages);
+
+  return galleryContainer;
+}
