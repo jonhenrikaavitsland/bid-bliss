952d04e6be064a2506aa7f965f8136c98d53ea76
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-27 09:30:04 +0200

    removes ability to scroll while on modal, and position remains

--- src/js/ui/modal/openModal.js
+++ src/js/ui/modal/openModal.js
@@ -3,9 +3,12 @@ import { modal } from '../../data/constants.js';
 
 export function openModal(target) {
   if (!target) closeModal(modal);
+  const scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
   modal.classList.remove('hidden');
   modal.classList.add('flex', 'items-center', 'justify-center', 'px-5');
+  modal.style.top = `${scrollPosition}px`;
+  document.body.classList.add('overflow-hidden');
   modal.append(target);
 
   modal.addEventListener('click', (event) => {
