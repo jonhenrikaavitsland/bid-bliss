0c3c5e1e38c73b220737f9311ad357ce12ff1257
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-28 11:45:06 +0200

    api key

--- src/js/data/constants.js
+++ src/js/data/constants.js
@@ -7,3 +7,4 @@ export const API_Register = '/register';
 export const API_Login = '/login';
 export const API_Listings = '/auction/listings';
 export const API_Profiles = '/auction/profiles';
+export const API_KEY = '469afe69-d51d-4f51-8e40-618b936a4e1a';
