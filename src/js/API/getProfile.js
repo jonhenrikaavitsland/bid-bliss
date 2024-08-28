da36455c050b09e0fc95eee92f8559288ddea814
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-28 17:30:08 +0200

    updates profile

---
+++ src/js/API/getProfile.js
@@ -0,0 +1,21 @@
+import { API_Base, API_Profiles } from '../data/constants';
+import { load } from '../localStorage/load';
+import { save } from '../localStorage/save';
+import { fetchData } from './fetchData';
+import { API_Key } from '../data/constants';
+
+export async function getProfile() {
+  const {
+    data: { name },
+  } = load('profile');
+
+  const profile = await fetchData(`${API_Base}${API_Profiles}/${name}`, {
+    headers: {
+      Authorization: `Bearer ${load('token')}`,
+      'X-Noroff-API-Key': API_Key,
+    },
+  });
+  console.log(profile);
+
+  save('profile', profile);
+}
