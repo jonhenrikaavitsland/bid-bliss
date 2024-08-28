ebffb3d6ae03ba3fb69f537923768d0ba9f6a0d0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-28 17:31:17 +0200

    updates profile

--- src/js/ui/loggedIn/ifLoggedIn.js
+++ src/js/ui/loggedIn/ifLoggedIn.js
@@ -1,8 +1,10 @@
+import { getProfile } from '../../API/getProfile';
 import { load } from '../../localStorage/load';
 import { addBtn } from './addBtn';
 
 export function ifLoggedIn() {
   if (load('token') && load('profile')) {
+    getProfile();
     addBtn();
     // Apply logged in menu
   }
