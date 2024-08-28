d67f4b4c151126f3a2ddaa9954b1fafd8ab4e6c1
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-28 10:10:19 +0200

    trying to fix overflow issues with the gallery modal, added shadow, blur issues corrected

--- src/js/template/galleryModal.js
+++ src/js/template/galleryModal.js
@@ -8,21 +8,21 @@ const DEFAULT_IMAGE_ALT = 'listing item';
 export function galleryModal() {
   const media = load('media');
 
-  const galleryContainer = createDiv('flex', 'flex-col', 'gap-10', 'overflow-y-auto', 'h-[90%]', 'max-h-screen');
+  const galleryContainer = createDiv('flex', 'flex-col', 'gap-10', 'max-w-screen', 'overflow-y-auto', 'max-h-screen', 'md:max-w-[500px]', 'lg:max-w-[700px]', 'shadow-customShadow');
 
   const activeImage = createImg(media[0].url || DEFAULT_IMAGE_URL, media[0].alt || DEFAULT_IMAGE_ALT, 'rounded-xl', 'aspect-square', 'object-contain', 'object-bottom');
 
-  const allImages = createDiv('flex', 'justify-center', 'gap-5', 'md:gap-8', 'lg:gap-10');
+  const allImages = createDiv('flex', 'justify-center', 'flex-wrap', 'gap-5', 'md:gap-8', 'lg:gap-10');
 
   media.forEach((image, index) => {
     const isActive = index === 0;
-    const imageObject = createImg(image.url || DEFAULT_IMAGE_URL, image.alt || DEFAULT_IMAGE_ALT, 'rounded-xl', 'cursor-pointer', 'max-w-24', 'aspect-square', 'object-cover');
-    if (isActive) imageObject.classList.add('blur-sm');
+    const imageObject = createImg(image.url || DEFAULT_IMAGE_URL, image.alt || DEFAULT_IMAGE_ALT, 'rounded-xl', 'cursor-pointer', 'max-w-10', 'aspect-square', 'object-cover', 'border-2', 'border-white', 'blur-sm');
+    if (isActive) imageObject.classList.remove('blur-sm');
     imageObject.addEventListener('click', () => {
       activeImage.src = image.url || DEFAULT_IMAGE_URL;
       activeImage.alt = image.alt || DEFAULT_IMAGE_ALT;
 
-      allImages.childNodes.forEach((img) => img.classList.add('blur-sm', 'border-2', 'border-white'));
+      allImages.childNodes.forEach((img) => img.classList.add('blur-sm'));
       imageObject.classList.remove('blur-sm');
     });
 
