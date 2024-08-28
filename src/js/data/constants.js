df486e0d0267b309023c71a1ab7e89793fe2b211
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-28 17:30:33 +0200

    new variable added

--- src/js/data/constants.js
+++ src/js/data/constants.js
@@ -5,6 +5,6 @@ export const API_Base = 'https://v2.api.noroff.dev';
 export const API_Auth = '/auth';
 export const API_Register = '/register';
 export const API_Login = '/login';
-export const API_Listings = '/auction/listings';
 export const API_Profiles = '/auction/profiles';
+export const API_Listings = '/auction/listings';
 export const API_Key = '469afe69-d51d-4f51-8e40-618b936a4e1a';
