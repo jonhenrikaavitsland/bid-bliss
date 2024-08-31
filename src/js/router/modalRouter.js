5859eb242ceac2b8cfc83e524f2ce1c6f2b9c05d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-31 18:30:00 +0200

    adding profile modal

--- src/js/router/modalRouter.js
+++ src/js/router/modalRouter.js
@@ -3,11 +3,13 @@ import { loginModal } from '../template/loginModal.js';
 import { registerModal } from '../template/registerModal.js';
 import { listingModal } from '../template/listingModal.js';
 import { galleryModal } from '../template/galleryModal.js';
+import { profileModal } from '../template/profileModal.js';
 
 let modalLogin;
 let modalRegister;
 let modalListing;
 let modalGallery;
+let modalProfile;
 
 export function modalRouter(modalValue) {
   switch (modalValue) {
@@ -23,6 +25,9 @@ export function modalRouter(modalValue) {
     case 'gallery':
       modalGallery = galleryModal();
       return modalGallery;
+    case 'profile':
+      modalProfile = profileModal();
+      return modalProfile;
     default:
       throw console.error('No modal were matched in the router!');
   }
