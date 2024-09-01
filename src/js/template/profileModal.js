79e181cb88b0817e4f9a44bee68fda62f932e3e9
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-01 18:35:02 +0200

    adjusting styles for larger screens

--- src/js/template/profileModal.js
+++ src/js/template/profileModal.js
@@ -23,27 +23,27 @@ export function profileModal() {
     _count: { listings, wins },
   } = load('profile');
 
-  const element = createDiv('bg-neutralBg', 'rounded-xl', 'pt-5', 'px-5', 'pb-10', 'shadow-customShadow', 'flex', 'flex-col', 'grow', 'gap-5');
+  const element = createDiv('bg-neutralBg', 'rounded-xl', 'pt-5', 'px-5', 'pb-10', 'md:pt-8', 'md:px-8', 'md:pb-16', 'shadow-customShadow', 'flex', 'flex-col', 'grow', 'gap-5', 'max-w-xl');
 
-  const image = createImg(url || DEFAULT_IMAGE_URL, alt || DEFAULT_IMAGE_ALT, 'w-28', 'rounded-full', 'mx-auto', 'border', 'border-secondary');
+  const image = createImg(url || DEFAULT_IMAGE_URL, alt || DEFAULT_IMAGE_ALT, 'w-28', 'md:w-40', 'rounded-full', 'mx-auto', 'border', 'border-secondary');
 
   const infoWrap = createDiv('flex', 'flex-col', 'gap-2.5', 'text-center');
 
   const nameWrap = createSection('flex', 'flex-col', 'gap-1', 'capitalize');
-  const title = createHeading(2, name, 'font-serif', 'font-medium');
-  const bioInfo = createParagraph(bio || DEFAULT_BIO, 'text-sm', 'italic');
+  const title = createHeading(2, name, 'font-serif', 'font-medium', 'md:text-lg', 'md:font-semibold');
+  const bioInfo = createParagraph(bio || DEFAULT_BIO, 'text-sm', 'italic', 'md:text-base');
 
-  const creditCount = createHeading(3, `Cr. ${credits}`, 'text-lg', 'font-semibold');
+  const creditCount = createHeading(3, `Cr. ${credits}`, 'text-lg', 'font-semibold', 'md:text-xl');
 
   const countWrap = createDiv('flex', 'flex-col', 'gap-1', 'capitalize', 'text-sm');
   const activeListings = createSpan(`active listings: ${listings}`);
   const winnings = createSpan(`listings won: ${wins}`);
 
   const uploadForm = createForm('upload', 'avatar', 'flex', 'flex-col', 'gap-1.5');
-  const uploadInfo = createLabel('uploadAvatar', 'Change avatar:', 'text-sm');
-  const uploadWrap = createDiv('flex');
-  const uploadInput = createInput('text', 'image URL', 'uploadAvatar', 'bg-white', 'rounded-s-xl', 'grow', 'ps-3', 'shadow-customShadow');
-  const uploadBtn = createBtn('upload', 'uppercase', 'rounded-e-xl', 'bg-primary', 'py-2', 'px-3', 'md:py-4', 'md:px-6', 'hover:bg-hoverPrimary', 'text-white', 'shadow-customShadow');
+  const uploadInfo = createLabel('uploadAvatar', 'Change avatar:', 'text-sm', 'md:text-base');
+  const uploadWrap = createDiv('flex', 'xsm:flex-col');
+  const uploadInput = createInput('text', 'image URL', 'uploadAvatar', 'bg-white', 'xsm:rounded-t-xl', 'xsm:px-3', 'xsm:py-2', 'xsm:text-center', 'sm:rounded-s-xl', 'grow', 'sm:ps-3', 'shadow-customShadow');
+  const uploadBtn = createBtn('upload', 'uppercase', 'xsm:rounded-b-xl', 'sm:rounded-e-xl', 'bg-primary', 'py-2', 'px-3', 'md:py-4', 'md:px-6', 'hover:bg-hoverPrimary', 'text-white', 'shadow-customShadow');
 
   const logOutBtn = createBtn('log out', 'bg-primary', 'hover:bg-hoverPrimary', 'rounded-xl', 'py-2', 'px-3', 'md:py-4', 'md:px-6', 'uppercase', 'text-white', 'mx-auto', 'mt-4', 'shadow-customShadow');
 
