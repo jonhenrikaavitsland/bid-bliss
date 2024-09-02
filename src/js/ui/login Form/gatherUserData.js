be2af01434d633a329d31e59990d6539debd5a95
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-03 00:12:29 +0200

    function now includes validation attemt 2.0

--- src/js/ui/login Form/gatherUserData.js
+++ src/js/ui/login Form/gatherUserData.js
@@ -1,13 +1,22 @@
 import { loginUser } from '../../API/loginUser';
 import { registerUser } from '../../API/registerUser';
-import { modal } from '../../data/constants';
+import { modal, MSG_Email, MSG_Password } from '../../data/constants';
+import { addLiveValidation } from '../../validate/addLiveValidation';
+import { updateValidation } from '../../validate/updateValidation';
+import { validateEmail } from '../../validate/validateEmail';
+import { validatePassword } from '../../validate/validatePassword';
 import { runModal } from '../modal/runModal';
 
 export async function gatherUserData() {
   const loginForm = document.querySelector(`[data-api="login"]`);
   const registerForm = document.querySelector(`[data-api="register"]`);
+  const emailMessage = modal.querySelector(`[data-validate="email"]`);
+  const passwordMessage = modal.querySelector(`[data-validate="password"]`);
+  let isEmailValid;
+  let isPasswordValid;
+
   if (registerForm) {
-    console.log(registerForm);
+    addLiveValidation(registerForm);
     registerForm.addEventListener('submit', async (event) => {
       event.preventDefault();
 
@@ -15,18 +24,40 @@ export async function gatherUserData() {
       const email = event.target.email.value;
       const password = event.target.password.value;
 
+      isEmailValid = validateEmail(email);
+      isPasswordValid = validatePassword(password);
+
+      updateValidation(event.target.email, isEmailValid, emailMessage, MSG_Email);
+
+      updateValidation(event.target.password, isPasswordValid, passwordMessage, MSG_Password);
+
+      if (!isEmailValid || !isPasswordValid) {
+        return;
+      }
+
       await registerUser(name, email, password);
       modal.innerHTML = '';
       runModal(true, 'login');
     });
   }
   if (loginForm) {
-    console.log(loginForm);
+    addLiveValidation(loginForm);
     loginForm.addEventListener('submit', async (event) => {
       event.preventDefault();
 
       const email = event.target.email.value;
       const password = event.target.password.value;
+
+      isEmailValid = validateEmail(email);
+      isPasswordValid = validatePassword(password);
+
+      updateValidation(event.target.email, isEmailValid, emailMessage, MSG_Email);
+
+      updateValidation(event.target.password, isPasswordValid, passwordMessage, MSG_Password);
+
+      if (!isEmailValid || !isPasswordValid) {
+        return;
+      }
 
       await loginUser(email, password);
 
