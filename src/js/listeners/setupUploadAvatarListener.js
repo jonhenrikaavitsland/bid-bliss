65308b3bec31b38434e13701a46d9f8a0bf8c758
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-04 00:36:58 +0200

    updates images upon successful reuqest

--- src/js/listeners/setupUploadAvatarListener.js
+++ src/js/listeners/setupUploadAvatarListener.js
@@ -2,8 +2,10 @@ import { updateProfile } from '../API/updateProfile';
 import { isValidUrl } from '../data/isValidUrl';
 
 export function setupUploadAvatarListener() {
-  const uploadBtn = document.querySelector(`[data-upload-btn]`);
-  const uploadInput = document.querySelector(`[data-upload-input]`);
+  const uploadBtn = document.querySelector(`[data-upload="btn"]`);
+  const uploadInput = document.querySelector(`[data-upload="input"]`);
+  const avatarImage = document.querySelector(`[data-avatar="img"]`);
+  const avatarBtn = document.querySelector(`[data-avatar="btn"]`);
 
   uploadBtn.addEventListener('click', async (event) => {
     event.preventDefault();
@@ -16,8 +18,12 @@ export function setupUploadAvatarListener() {
       return;
     }
 
-    const success = await updateProfile(url);
-    if (success) {
+    const updatedProfile = await updateProfile(url);
+    if (updateProfile) {
+      avatarImage.src = url;
+      avatarImage.alt = updateProfile.name;
+      avatarBtn.src = url;
+      avatarBtn.alt = updateProfile.name;
       uploadInput.classList.remove('border-error');
       uploadInput.classList.add('border-2', 'border-correct');
     }
