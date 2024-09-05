464ed117624740990259317b315e4fea88230284
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-05 20:42:44 +0200

    updating function to include id

--- src/js/elements/createTextarea.js
+++ src/js/elements/createTextarea.js
@@ -1,5 +1,6 @@
-export function createTextarea(placeholder = '', rows = 4, ...classes) {
+export function createTextarea(id, placeholder = '', rows = 4, ...classes) {
   const element = document.createElement('textarea');
+  element.setAttribute('id', id);
   element.placeholder = placeholder;
   element.rows = rows;
   element.classList.add(...classes);
