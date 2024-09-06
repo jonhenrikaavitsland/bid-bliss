83889022bdfb760e7cd75ff7a3aeb45f23fb586f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-06 23:55:55 +0200

    adding condition check

--- src/js/validate/validateBid.js
+++ src/js/validate/validateBid.js
@@ -1,11 +1,11 @@
 import { load } from '../localStorage/load';
 
-export function validateBid(bid) {
+export function validateBid(bid, currentHigh) {
   const profile = load('profile');
   const { credits } = profile;
   const bidAmount = parseFloat(bid.value);
 
-  if (isNaN(bidAmount) || bidAmount <= 0 || bidAmount > credits) {
+  if (isNaN(bidAmount) || bidAmount <= 0 || bidAmount > credits || bidAmount <= currentHigh) {
     bid.classList.add('border-2', 'border-error');
     bid.classList.remove('border-correct');
   } else {
