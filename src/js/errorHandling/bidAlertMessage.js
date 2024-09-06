89e4cb44a0f3eac3f65a0a20efe65faf8572cdfa
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-06 23:41:47 +0200

    bid alert messages

---
+++ src/js/errorHandling/bidAlertMessage.js
@@ -0,0 +1,13 @@
+export function bidAlertMessage(alertStatus) {
+  let message;
+
+  switch (alertStatus) {
+    case 400: // Unauthorized
+      message = 'Your bid must be higher than the current bid.';
+      break;
+    default: // Any other status
+      message = 'An unknown error occurred. Please try again.';
+      break;
+  }
+  return message;
+}
