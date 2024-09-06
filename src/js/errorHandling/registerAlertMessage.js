fc221ef3587c4561d51c8112dfbfc0959e0397b4
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-06 23:18:11 +0200

    removing exccessive alerts

--- src/js/errorHandling/registerAlertMessage.js
+++ src/js/errorHandling/registerAlertMessage.js
@@ -8,18 +8,6 @@ export function registerAlertMessage(alertStatus) {
     case 400: // Bad Request
       message = 'An account with this email already exists. Please use a different email.';
       break;
-    case 401: // Unauthorized
-      message = 'You are not authorized to perform this action. Please ensure you are logged in properly.';
-      break;
-    case 429: // Too Many Requests
-      message = 'You have made too many requests in a short time. Please wait a moment and try again.';
-      break;
-    case 500: // Internal Server Error
-      message = 'An unexpected error occurred. Please try again later.';
-      break;
-    case 503: // Service Unavailable
-      message = 'The service is currently unavailable. Please try again later.';
-      break;
     default: // Any other status
       message = 'An unknown error occurred. Please try again.';
       break;
