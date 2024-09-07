f18c4634d2a4738b502aaf54da12e8ffddc2020a
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-07 03:28:14 +0200

    added a 201 case

--- src/js/errorHandling/bidAlertMessage.js
+++ src/js/errorHandling/bidAlertMessage.js
@@ -2,6 +2,8 @@ export function bidAlertMessage(alertStatus) {
   let message;
 
   switch (alertStatus) {
+    case 201:
+      break;
     case 400: // Unauthorized
       message = 'Your bid must be higher than the current bid.';
       break;
