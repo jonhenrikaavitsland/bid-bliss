2672d78ca5310b76d6c934eafe1a84fbee587b37
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-07 08:32:05 +0200

    now running the initializeListings function

--- src/js/data/listings.js
+++ src/js/data/listings.js
@@ -1,8 +1,3 @@
-import { fetchData } from '../API/fetchData';
-import { API_Base, API_Listings } from './constants';
-import { sortListings } from './sortListings';
+import { initializeListings } from './initializeListings';
 
-export const listings = await fetchData(`${API_Base}${API_Listings}?_seller=true&_bids=true`);
-
-// Sorts the array
-export const sortedListings = sortListings(listings);
+export let listings = await initializeListings();
