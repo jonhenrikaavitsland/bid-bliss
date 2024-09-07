fb977fb9271606ddcb28925416bacfed47c1d5f2
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-07 06:14:06 +0200

    sorting the array

--- src/js/data/listings.js
+++ src/js/data/listings.js
@@ -1,4 +1,8 @@
 import { fetchData } from '../API/fetchData';
 import { API_Base, API_Listings } from './constants';
+import { sortListings } from './sortListings';
 
 export const listings = await fetchData(`${API_Base}${API_Listings}?_seller=true&_bids=true`);
+
+// Sorts the array
+export const sortedListings = sortListings(listings);
