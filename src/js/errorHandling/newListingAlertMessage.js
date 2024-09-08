a3757f9dd0eb1af0b586a5e2c69e1b05b928da1d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 08:59:55 +0200

    added 201 case

--- src/js/errorHandling/newListingAlertMessage.js
+++ src/js/errorHandling/newListingAlertMessage.js
@@ -2,6 +2,9 @@ export function newListingAlertMessage(alertStatus) {
   let message;
 
   switch (alertStatus) {
+    case 201: // Success - Listing created
+      message = 'test'; // No alert needed for successful listing creation
+      break;
     case 400: // Unauthorized
       message = 'Image URL cannot be greater than 300 characters';
       break;
