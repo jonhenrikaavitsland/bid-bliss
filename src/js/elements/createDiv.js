3e87ca8b4c0543481ac04891ec05a0878c29f8e6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 07:32:35 +0200

    improves code, validates classes

--- src/js/elements/createDiv.js
+++ src/js/elements/createDiv.js
@@ -1,5 +1,11 @@
 export function createDiv(...classes) {
   const div = document.createElement('div');
-  div.classList.add(...classes);
+
+  const validClasses = classes.filter((cls) => typeof cls === 'string' && cls.trim() !== '');
+
+  if (validClasses.length > 0) {
+    div.classList.add(...validClasses);
+  }
+
   return div;
 }
