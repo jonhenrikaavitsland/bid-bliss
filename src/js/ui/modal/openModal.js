4c44e22308a7a59c255d3c023121772cfc0d5cad
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 13:03:17 +0200

    readability, added return

--- src/js/ui/modal/openModal.js
+++ src/js/ui/modal/openModal.js
@@ -2,20 +2,24 @@ import { closeModal } from './closeModal.js';
 import { modal } from '../../data/constants.js';
 
 export function openModal(target) {
-  if (!target) closeModal(modal);
+  if (!target) {
+    closeModal(modal);
+    return;
+  }
+
   const scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
   modal.classList.remove('hidden');
   modal.classList.add('flex', 'items-center', 'justify-center', 'px-5');
+
   modal.style.top = `${scrollPosition}px`;
   document.body.classList.add('overflow-hidden');
+
   modal.append(target);
 
   modal.addEventListener('click', (event) => {
     if (event.target === modal) {
       closeModal(modal);
-    } else {
-      return;
     }
   });
 }
