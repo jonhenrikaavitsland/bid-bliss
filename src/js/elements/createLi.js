3717b732f84d861803a8ce3e42c65f7e0a57f0de
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 08:05:18 +0200

    validates classes

--- src/js/elements/createLi.js
+++ src/js/elements/createLi.js
@@ -1,5 +1,11 @@
 export function createLi(...classes) {
   const element = document.createElement('li');
-  element.classList.add(...classes);
+
+  const validClasses = classes.filter((cls) => typeof cls === 'string' && cls.trim() !== '');
+
+  if (validClasses.length > 0) {
+    element.classList.add(...validClasses);
+  }
+
   return element;
 }
