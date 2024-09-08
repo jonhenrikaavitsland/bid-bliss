0e75ebd91bd6a2052adb21e3a096736170dd780e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 08:38:30 +0200

    adds null to 201

--- src/js/errorHandling/bidAlertMessage.js
+++ src/js/errorHandling/bidAlertMessage.js
@@ -3,14 +3,15 @@ export function bidAlertMessage(alertStatus) {
 
   switch (alertStatus) {
     case 201:
+      message = null;
       break;
-    case 400: // Unauthorized
+    case 400:
       message = 'Your bid must be higher than the current bid.';
       break;
     case 403:
       message = 'You cannot bid on your own listing';
       break;
-    default: // Any other status
+    default:
       message = 'An unknown error occurred. Please try again.';
       break;
   }
