42f0e63c2e592d50bd70c990bfad8d4b5c0ba71e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 10:26:56 +0200

    improved errorhandling

--- src/js/listeners/setupUploadAvatar/updateImageElements.js
+++ src/js/listeners/setupUploadAvatar/updateImageElements.js
@@ -3,10 +3,24 @@ export function updateImageElements(url, altText) {
   const avatarBtn = document.querySelector('[data-avatar="btn"]');
   const desktopAvatarImage = document.querySelector('[data-avatar="desktop"]');
 
-  avatarImage.src = url;
-  avatarImage.alt = altText;
-  avatarBtn.src = url;
-  avatarBtn.alt = altText;
-  desktopAvatarImage.src = url;
-  desktopAvatarImage.alt = altText;
+  if (avatarImage) {
+    avatarImage.src = url;
+    avatarImage.alt = altText;
+  } else {
+    console.error('Avatar image element not found');
+  }
+
+  if (avatarBtn) {
+    avatarBtn.src = url;
+    avatarBtn.alt = altText;
+  } else {
+    console.error('Avatar button element not found');
+  }
+
+  if (desktopAvatarImage) {
+    desktopAvatarImage.src = url;
+    desktopAvatarImage.alt = altText;
+  } else {
+    console.error('Desktop avatar image element not found');
+  }
 }
