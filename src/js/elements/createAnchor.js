df16d1351da8f32104d24d9bcd6bed2767b30ef0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 07:25:47 +0200

    improving the code with url validation, improves security, ensures classes are valid, and default text content if no content

--- src/js/elements/createAnchor.js
+++ src/js/elements/createAnchor.js
@@ -1,8 +1,19 @@
+import { isValidUrl } from '../data/isValidUrl';
+
 export function createAnchor(href, textContent, target = '_self', ...classes) {
   const anchor = document.createElement('a');
-  anchor.href = href;
-  anchor.textContent = textContent;
+  anchor.href = isValidUrl(href) ? href : '#';
+  anchor.textContent = textContent || 'link';
   anchor.target = target;
-  anchor.classList.add(...classes);
+
+  if (target === '_blank') {
+    anchor.rel = 'noopener noreferrer';
+  }
+
+  const validClasses = classes.filter((cls) => typeof cls === 'string' && cls.trimEnd() !== '');
+  if (validClasses.length) {
+    anchor.classList.add(...validClasses);
+  }
+
   return anchor;
 }
