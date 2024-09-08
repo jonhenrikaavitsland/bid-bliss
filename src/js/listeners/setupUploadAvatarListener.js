a36399768f0c49479647a6886ae5ec7a4f11a6ca
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 10:04:46 +0200

    added error handling and event listner removal

--- src/js/listeners/setupUploadAvatarListener.js
+++ src/js/listeners/setupUploadAvatarListener.js
@@ -2,9 +2,20 @@ import { handleInputChange } from './setupUploadAvatar/handleInputChange';
 import { handleUploadClick } from './setupUploadAvatar/handleUploadClick';
 
 export function setupUploadAvatarListener() {
-  const uploadBtn = document.querySelector('[data-upload="btn"]');
+  const uploadButton = document.querySelector('[data-upload="btn"]');
   const uploadInput = document.querySelector('[data-upload="input"]');
 
-  uploadBtn.addEventListener('click', handleUploadClick);
-  uploadInput.addEventListener('input', handleInputChange);
+  if (uploadButton) {
+    uploadButton.removeEventListener('click', handleUploadClick); // Remove existing listener
+    uploadButton.addEventListener('click', handleUploadClick); // Add new listener
+  } else {
+    console.error('Upload button not found');
+  }
+
+  if (uploadInput) {
+    uploadInput.removeEventListener('input', handleInputChange); // Remove existing listener
+    uploadInput.addEventListener('input', handleInputChange); // Add new listener
+  } else {
+    console.error('Upload input not found');
+  }
 }
