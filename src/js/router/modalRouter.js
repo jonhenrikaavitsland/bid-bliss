501a822cacdb6f5967ddd8b49bb2a5def9245aa0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 10:59:13 +0200

    improve readability and removed the used of let variables to avoid potential issues with state management

--- src/js/router/modalRouter.js
+++ src/js/router/modalRouter.js
@@ -6,34 +6,61 @@ import { galleryModal } from '../template/galleryModal';
 import { profileModal } from '../template/profileModal';
 import { newListingModal } from '../template/newListingModal';
 
-let modalLogin;
-let modalRegister;
-let modalListing;
-let modalGallery;
-let modalProfile;
-let modalNewListing;
-
 export function modalRouter(modalValue) {
   switch (modalValue) {
     case 'login':
-      modalLogin = loginModal();
-      return modalLogin;
+      return loginModal();
     case 'register':
-      modalRegister = registerModal();
-      return modalRegister;
+      return registerModal();
     case 'listing':
-      modalListing = listingModal(listings);
-      return modalListing;
+      return listingModal(listings);
     case 'gallery':
-      modalGallery = galleryModal();
-      return modalGallery;
+      return galleryModal();
     case 'profile':
-      modalProfile = profileModal();
-      return modalProfile;
+      return profileModal();
     case 'newListing':
-      modalNewListing = newListingModal();
-      return modalNewListing;
+      return newListingModal();
     default:
-      throw console.error('No modal were matched in the router!');
+      throw new Error(`No modal matched for the value: ${modalValue}`);
   }
 }
+
+// import { listings } from '../data/listings';
+// import { loginModal } from '../template/loginModal';
+// import { registerModal } from '../template/registerModal';
+// import { listingModal } from '../template/listingModal';
+// import { galleryModal } from '../template/galleryModal';
+// import { profileModal } from '../template/profileModal';
+// import { newListingModal } from '../template/newListingModal';
+
+// let modalLogin;
+// let modalRegister;
+// let modalListing;
+// let modalGallery;
+// let modalProfile;
+// let modalNewListing;
+
+// export function modalRouter(modalValue) {
+//   switch (modalValue) {
+//     case 'login':
+//       modalLogin = loginModal();
+//       return modalLogin;
+//     case 'register':
+//       modalRegister = registerModal();
+//       return modalRegister;
+//     case 'listing':
+//       modalListing = listingModal(listings);
+//       return modalListing;
+//     case 'gallery':
+//       modalGallery = galleryModal();
+//       return modalGallery;
+//     case 'profile':
+//       modalProfile = profileModal();
+//       return modalProfile;
+//     case 'newListing':
+//       modalNewListing = newListingModal();
+//       return modalNewListing;
+//     default:
+//       throw console.error('No modal were matched in the router!');
+//   }
+// }
