a0ba229a7f74ba378d3e92bc25706d9321b00c41
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-08 10:19:48 +0200

    improved errorhandling

--- src/js/listeners/setupUploadAvatar/handleInputChange.js
+++ src/js/listeners/setupUploadAvatar/handleInputChange.js
@@ -4,6 +4,12 @@ import { updateInputStyles } from './updateInputStyles';
 
 export function handleInputChange() {
   const uploadInput = document.querySelector('[data-upload="input"]');
+
+  if (!uploadInput) {
+    console.error('Upload input element not found');
+    return;
+  }
+
   const url = uploadInput.value.trim();
 
   if (!url) {
