238ddf07d19dd702bb23d12fff7d36cd672f8e1f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-10 19:25:51 +0200

    we now log in only when we have successfully logged in

--- src/js/errorHandling/loginAlertMessage.js
+++ src/js/errorHandling/loginAlertMessage.js
@@ -1,14 +1,20 @@
+import { ifLoggedIn } from '../ui/loggedIn/ifLoggedIn';
+import { closeModal } from '../ui/modal/closeModal';
+import { modal } from '../data/constants';
+
 export function loginAlertMessage(alertStatus) {
   let message;
 
   switch (alertStatus) {
     case 200:
       message = null;
+      setTimeout(ifLoggedIn, 1000);
+      closeModal(modal);
       break;
-    case 401: // Unauthorized
+    case 401:
       message = 'Incorrect email or password. Please try again.';
       break;
-    default: // Any other status
+    default:
       message = 'An unknown error occurred during login. Please try again.';
       break;
   }
