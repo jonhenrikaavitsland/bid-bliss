7c7cc09262a37a7969a0e8abdc3c7fac8ad09b9c
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-10 15:58:43 +0200

    imports images, then exports them

---
+++ src/js/data/images.js
@@ -0,0 +1,7 @@
+import placeholderImg from '../../images/placeholder.jpg';
+import placeholderItemImg from '../../images/placeholderItem.png';
+import closeSvg from '../../images/close.svg';
+import auctioneerWelcomeImg from '../../images/auctioneer-welcome.png';
+import auctioneerSignupImg from '../../images/auctioneer-signup.png';
+
+export { placeholderImg, placeholderItemImg, closeSvg, auctioneerWelcomeImg, auctioneerSignupImg };
