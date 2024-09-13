1baa1789c33738d3aacde6be912e897ca6eb1703
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-13 17:41:32 +0200

    jsdocs upload avatar upload click

--- src/js/listeners/setupUploadAvatar/handleUploadClick.js
+++ src/js/listeners/setupUploadAvatar/handleUploadClick.js
@@ -3,6 +3,23 @@ import { isImageAccessible } from '../../API/isImageAccessible';
 import { handleInvalidUrl } from './handleInvalidUrl';
 import { updateAvatar } from './updateAvatar';
 
+/**
+ * Handles the click event for the upload button, validating the URL and updating the avatar if valid.
+ *
+ * This function prevents the default button behavior, validates the image URL from the input field,
+ * checks if the image is accessible, and updates the avatar. If the URL is invalid or the image is inaccessible,
+ * it handles the error by updating input styles and showing an alert message. Logs errors if encountered during the process.
+ *
+ * @async
+ * @param {Event} event The event object from the click event.
+ * @returns {Promise<void>} No return value; performs actions based on URL validity.
+ * @example
+ * ```js
+ * // Set up a click event listener for the upload button
+ * const uploadButton = document.querySelector('[data-upload="btn"]');
+ * uploadButton.addEventListener('click', handleUploadClick);
+ * ```
+ */
 export async function handleUploadClick(event) {
   event.preventDefault();
 
