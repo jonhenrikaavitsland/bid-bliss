ded2b868be8c87f8384b1dd42448562b6b01cb58
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-13 20:20:18 +0200

    jsdocs nav links

--- src/js/template/navLinks.js
+++ src/js/template/navLinks.js
@@ -4,6 +4,21 @@ import { createUl } from '../elements/createUl';
 import { logout } from '../ui/logoutBtn/logout';
 import { runModal } from '../ui/modal/runModal';
 
+/**
+ * Creates and returns a navigation element with links for logged-in users.
+ *
+ * This function generates a list of navigation buttons, including "New Auction," "Profile," and "Logout."
+ * Each button is wrapped in a list item and is styled for navigation. Event listeners are attached to handle
+ * modal openings and logout functionality based on the button clicked.
+ *
+ * @returns {HTMLElement} The constructed unordered list element containing navigation buttons.
+ * @example
+ * ```js
+ * // Create navigation links and append them to the navigation bar
+ * const nav = navLinks();
+ * document.querySelector('nav').append(nav);
+ * ```
+ */
 export function navLinks() {
   const element = createUl('hidden', 'lg:flex', 'order-first', 'font-serif', 'gap-5', 'text-white', 'text-xl');
 
