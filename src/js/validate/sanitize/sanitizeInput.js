3fbfaac1901d4e6defffb16591f114b8718e8d2d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-13 20:51:44 +0200

    jsdocs sanitize input

--- src/js/validate/sanitize/sanitizeInput.js
+++ src/js/validate/sanitize/sanitizeInput.js
@@ -1,5 +1,21 @@
 import { createDiv } from '../../elements/createDiv';
 
+/**
+ * Sanitizes a given input string by encoding HTML entities to prevent XSS attacks.
+ *
+ * This function creates a temporary div element, sets its inner text to the input string, and retrieves the sanitized HTML,
+ * effectively escaping any potentially dangerous HTML tags or scripts.
+ *
+ * @param {string} input The input string to be sanitized.
+ * @returns {string} The sanitized string with HTML entities encoded.
+ * @example
+ * ```js
+ * // Sanitize a potentially unsafe input
+ * const userInput = '<script>alert("XSS")</script>';
+ * const safeInput = sanitizeInput(userInput);
+ * console.log(safeInput); // Outputs: "&lt;script&gt;alert(&quot;XSS&quot;)&lt;/script&gt;"
+ * ```
+ */
 export function sanitizeInput(input) {
   const div = createDiv();
   div.innerText = input;
