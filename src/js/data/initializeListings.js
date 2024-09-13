d66369b0e2292fb6c7a8745973826baf481e3a80
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-13 15:27:53 +0200

    jsdocs initialize listings

--- src/js/data/initializeListings.js
+++ src/js/data/initializeListings.js
@@ -1,6 +1,26 @@
 import { listingService } from './listingService';
 import { sortListings } from './sortListings';
 
+/**
+ * Initializes listings by fetching and sorting them.
+ *
+ * Fetches listings using the `listingService` and then sorts the fetched listings using the `sortListings` function.
+ * If no listings are found, an empty array is sorted and returned.
+ *
+ * @async
+ * @returns {Promise<Array>} A promise that resolves to an array of sorted listings.
+ * @example
+ * ```js
+ * // Initialize and sort listings
+ * initializeListings()
+ *   .then(sortedListings => {
+ *     console.log('Listings initialized and sorted:', sortedListings);
+ *   })
+ *   .catch(error => {
+ *     console.error('Error initializing listings:', error);
+ *   });
+ * ```
+ */
 export async function initializeListings() {
   await listingService.fetchListings();
   const listings = listingService.getListings() || [];
