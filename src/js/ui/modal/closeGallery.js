efe342f6cd131d81c4940499a9b06ece687a18ac
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-13 14:39:37 +0200

    users should no longer be able to accidentally open a new modal when closing the modal

--- src/js/ui/modal/closeGallery.js
+++ src/js/ui/modal/closeGallery.js
@@ -2,4 +2,8 @@ export function closeGallery(gallery) {
   gallery.classList.remove('flex', 'items-center', 'justify-center', 'py-5');
   gallery.classList.add('hidden');
   gallery.innerHTML = '';
+  document.body.classList.add('pointer-events-none');
+  setTimeout(() => {
+    document.body.classList.remove('pointer-events-none');
+  }, 1000);
 }
