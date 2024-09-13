27bf54bae791fbde4bc6302ef22c64a8aa081824
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-13 12:18:31 +0200

    added close btn to the gallery

--- src/js/template/galleryModal.js
+++ src/js/template/galleryModal.js
@@ -1,7 +1,10 @@
-import { placeholderItemImg } from '../data/images';
+import { gallery } from '../data/constants';
+import { closeSvg, placeholderItemImg } from '../data/images';
+import { createBtn } from '../elements/createBtn';
 import { createDiv } from '../elements/createDiv';
 import { createImg } from '../elements/createImg';
 import { load } from '../localStorage/load';
+import { closeGallery } from '../ui/modal/closeGallery';
 
 const DEFAULT_IMAGE_URL = placeholderItemImg;
 const DEFAULT_IMAGE_ALT = 'listing item';
@@ -13,7 +16,7 @@ export function galleryModal() {
     console.warn('No media available, using default image.');
   }
 
-  const galleryContainer = createDiv('flex', 'flex-col', 'gap-10', 'max-w-screen', 'overflow-y-auto', 'max-h-full', 'md:max-w-[500px]', 'lg:max-w-[700px]', 'bg-neutralTxt', 'py-5');
+  const galleryContainer = createDiv('flex', 'flex-col', 'gap-10', 'max-w-screen', 'overflow-y-auto', 'max-h-full', 'md:max-w-[500px]', 'lg:max-w-[700px]', 'bg-neutralTxt', 'py-5', 'relative');
 
   const activeImage = createImg(media?.[0]?.url || DEFAULT_IMAGE_URL, media?.[0]?.alt || DEFAULT_IMAGE_ALT, 'aspect-square', 'object-contain', 'object-bottom', 'max-w-full', 'max-h-[60vh]');
 
@@ -40,8 +43,16 @@ export function galleryModal() {
     fragment.append(imageObject);
   });
 
+  const closeBtn = createBtn('', 'absolute', 'top-2.5', 'right-2.5', 'rounded-full', 'shadow-customShadow', 'hover:animate-pulse');
+  const closeImg = createImg(closeSvg, 'close', 'size-5');
+  closeBtn.append(closeImg);
+  closeBtn.addEventListener('click', () => {
+    closeGallery(gallery);
+  });
+
+  closeBtn.append(closeImg);
   allImages.appendChild(fragment);
-  galleryContainer.append(activeImage, allImages);
+  galleryContainer.append(activeImage, allImages, closeBtn);
 
   return galleryContainer;
 }
