3a9a3d95883a7eb85346cf4db8344cdce7afe97d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-13 15:05:17 +0200

    jsdocs

--- src/js/API/fetchData.js
+++ src/js/API/fetchData.js
@@ -1,5 +1,24 @@
 import { alertUser } from '../errorHandling/alertUser';
 
+/**
+ * Fetches data from a specified URL with optional configuration object and alert type.
+ *
+ * @param {string} url The URL to fetch data from.
+ * @param {Object} [object={}] Optional fetch configuration object (e.g., headers, method).
+ * @param {string} [alertType] Optional alert type to determine how to alert the user on response.
+ * @returns {Promise<Object>} A promise that resolves to the fetched data as a JSON object.
+ * @throws {Error} Throws an error if the URL is invalid or the fetch request fails.
+ * @example
+ * ```js
+ * // Fetch data from an API
+ * const url = 'https://api.example.com/data';
+ * const options = { method: 'GET' };
+ * const alertType = 'success';
+ * fetchData(url, options, alertType)
+ *   .then(data => console.log(data))
+ *   .catch(error => console.error(error));
+ * ```
+ */
 export async function fetchData(url, object = {}, alertType) {
   if (typeof url !== 'string' || !url.trim()) {
     console.error('Invalid URL provided');
