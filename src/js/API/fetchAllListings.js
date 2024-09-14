175279fa4580b0c3293af1d077f45e8869224918
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-14 15:59:11 +0200

    jsdocs for the new function

--- src/js/API/fetchAllListings.js
+++ src/js/API/fetchAllListings.js
@@ -1,6 +1,21 @@
 import { fetchData } from './fetchData';
 import { API_Base, API_Listings } from '../data/constants';
 
+/**
+ * Fetches all listings from the API by iterating through paginated results until no more listings are available.
+ *
+ * This function requests listings from the API page by page, concatenating the results into a single array.
+ * It continues fetching until a page returns fewer than 100 listings, indicating the end of available data.
+ *
+ * @async
+ * @returns {Promise<Array>} A promise that resolves to an array of all fetched listings.
+ * @throws Will throw an error if fetching listings fails.
+ * @example
+ * ```js
+ * // Fetch all listings from the API
+ * fetchAllListings().then(listings => console.log(listings));
+ * ```
+ */
 export async function fetchAllListings() {
   let page = 1;
   let allListings = [];
