760b85f5e83fe0c2eff55f3ffe51855c5b1ea1ae
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-16 00:33:01 +0200

    new login modal layout

--- src/js/template/loginModal.js
+++ src/js/template/loginModal.js
@@ -1,43 +1,64 @@
-import { modal } from '../data/constants.js';
-import { auctioneerWelcomeImg, closeSvg } from '../data/images.js';
-import { createAnchor } from '../elements/createAnchor.js';
-import { createBtn } from '../elements/createBtn.js';
-import { createDiv } from '../elements/createDiv.js';
-import { createForm } from '../elements/createForm.js';
-import { createHeading } from '../elements/createHeading.js';
-import { createImg } from '../elements/createImg.js';
-import { createInput } from '../elements/createInput.js';
-import { createLabel } from '../elements/createLabel.js';
-import { createSection } from '../elements/createSection.js';
-import { createSpan } from '../elements/createSpan.js';
-import { createTextNode } from '../elements/createTextNode.js';
-import { closeModal } from '../ui/modal/closeModal.js';
+import { modal } from '../data/constants';
+import { auctioneerWelcomeImg, closeSvg } from '../data/images';
+import { createAnchor } from '../elements/createAnchor';
+import { createBtn } from '../elements/createBtn';
+import { createDiv } from '../elements/createDiv';
+import { createForm } from '../elements/createForm';
+import { createHeading } from '../elements/createHeading';
+import { createImg } from '../elements/createImg';
+import { createInput } from '../elements/createInput';
+import { createLabel } from '../elements/createLabel';
+import { createSpan } from '../elements/createSpan';
+import { createTextNode } from '../elements/createTextNode';
+import { closeModal } from '../ui/modal/closeModal';
 
-/**
- * Creates and returns a login modal element for user authentication.
- *
- * This function constructs a login modal with an image, heading, form fields for email and password,
- * and a call-to-action button for logging in. It includes validation messages and a link for users to register if they don't have an account.
- * The modal can be closed by clicking the close button.
- *
- * @returns {HTMLElement} The constructed login modal element.
- * @example
- * ```js
- * // Create a login modal and append it to the document body
- * const modal = loginModal();
- * document.body.append(modal);
- * ```
- */
 export function loginModal() {
-  const element = createSection('relative', 'flex', 'flex-col', 'bg-neutralBg', 'p-8', 'rounded-xl', 'lg:pt-10', 'lg:px-10', 'pb-16', 'shadow-customShadow', 'overflow-y-auto', 'max-h-[90%]', 'max-h-screen');
+  const element = createDiv(
+    'flex',
+    'flex-col',
+    'px-2.5',
+    'md:px-3',
+    'pt-2.5',
+    'pb-9',
+    'md:pb-6',
+    'bg-neutralBg',
+    'rounded-xl',
+    'shadow-customShadow',
+    'min-w-40',
+    'max-w-96',
+    'my-auto',
+    'flex-grow',
+    'flex-shrink',
+    'xmd:landscape:max-w-[804px]',
+    'md:landscape:max-w-[1112px]',
+    'md:max-w-[672px]',
+  );
+  element.setAttribute('id', 'profileModal');
 
-  const imageWrap = createDiv('max-w-96', 'mx-auto');
+  const closeBtn = createBtn('', 'backdrop-invert', 'rounded-full', 'shadow-customShadow', 'hover:animate-pulse');
+  const closeImg = createImg(closeSvg, 'close', 'size-5');
+  closeBtn.append(closeImg);
+  const btnWrap = createDiv('size-9', 'flex', 'justify-center', 'items-center', 'cursor-pointer');
+  btnWrap.addEventListener('click', () => {
+    closeModal(modal);
+  });
+  btnWrap.append(closeBtn);
+  const btnContainer = createDiv('flex', 'justify-end');
+  btnContainer.append(btnWrap);
+
+  const topContainer = createDiv('flex', 'flex-col', 'landscape:w-1/2', 'px-5.5', 'landscape:ps-2.5', 'landscape:pe-5', 'landscape:my-auto');
+
+  const imageWrap = createDiv('max-w-full', 'mx-auto');
   const image = createImg(auctioneerWelcomeImg, 'auctioneer greeting');
   imageWrap.append(image);
 
-  const heading = createHeading(2, `"Your Next Great Deal Awaits!"`, 'font-serif', 'text-primary', 'text-center', 'mt-0.5', 'lg:mt-1', 'font-semibold', 'uppercase', 'md:text-xxl');
+  const heading = createHeading(2, `"Your Next Great Deal Awaits!"`, 'font-serif', 'text-center', 'mt-0.5', 'md:mt-1', 'font-semibold', 'uppercase', 'md:text-xl');
 
-  const formElement = createForm('api', 'login', 'flex', 'flex-col', 'gap-5', 'mt-9', 'mb-16', 'text-primary', 'md:gap-7');
+  topContainer.append(imageWrap, heading);
+
+  const bottomContainer = createDiv('flex', 'flex-col', 'landscape:w-1/2', 'landscape:justify-center', 'px-5.5', 'landscape:pe-2.5', 'landscape:ps-5');
+
+  const formElement = createForm('api', 'login', 'flex', 'flex-col', 'gap-5', 'mt-9', 'mb-16', 'md:gap-7');
 
   const emailGroup = createDiv('flex', 'flex-col', 'gap-1.5');
   const labelEmail = createLabel('email', 'Email:', 'md:text-xl');
@@ -67,15 +88,94 @@ export function loginModal() {
 
   ctaGroup.append(regCta);
 
-  const closeBtn = createBtn('', 'absolute', 'top-2.5', 'right-2.5', 'backdrop-invert', 'rounded-full', 'shadow-customShadow', 'hover:animate-pulse');
-  const closeImg = createImg(closeSvg, 'close', 'size-5');
-  closeBtn.append(closeImg);
-  closeBtn.addEventListener('click', () => {
-    closeModal(modal);
-  });
-
   formElement.append(emailGroup, passwordGroup, wrap);
-  element.append(imageWrap, heading, formElement, ctaGroup, closeBtn);
+  bottomContainer.append(formElement, ctaGroup);
 
+  const containers = createDiv('flex', 'flex-col', 'landscape:flex-row');
+  containers.append(topContainer, bottomContainer);
+
+  element.append(btnContainer, containers);
   return element;
 }
+
+// import { modal } from '../data/constants.js';
+// import { auctioneerWelcomeImg, closeSvg } from '../data/images.js';
+// import { createAnchor } from '../elements/createAnchor.js';
+// import { createBtn } from '../elements/createBtn.js';
+// import { createDiv } from '../elements/createDiv.js';
+// import { createForm } from '../elements/createForm.js';
+// import { createHeading } from '../elements/createHeading.js';
+// import { createImg } from '../elements/createImg.js';
+// import { createInput } from '../elements/createInput.js';
+// import { createLabel } from '../elements/createLabel.js';
+// import { createSection } from '../elements/createSection.js';
+// import { createSpan } from '../elements/createSpan.js';
+// import { createTextNode } from '../elements/createTextNode.js';
+// import { closeModal } from '../ui/modal/closeModal.js';
+
+// /**
+//  * Creates and returns a login modal element for user authentication.
+//  *
+//  * This function constructs a login modal with an image, heading, form fields for email and password,
+//  * and a call-to-action button for logging in. It includes validation messages and a link for users to register if they don't have an account.
+//  * The modal can be closed by clicking the close button.
+//  *
+//  * @returns {HTMLElement} The constructed login modal element.
+//  * @example
+//  * ```js
+//  * // Create a login modal and append it to the document body
+//  * const modal = loginModal();
+//  * document.body.append(modal);
+//  * ```
+//  */
+// export function loginModal() {
+//   const element = createSection('relative', 'flex', 'flex-col', 'bg-neutralBg', 'p-8', 'rounded-xl', 'lg:pt-10', 'lg:px-10', 'pb-16', 'shadow-customShadow', 'overflow-y-auto', 'max-h-[90%]', 'max-h-screen');
+
+//   const imageWrap = createDiv('max-w-96', 'mx-auto');
+//   const image = createImg(auctioneerWelcomeImg, 'auctioneer greeting');
+//   imageWrap.append(image);
+
+//   const heading = createHeading(2, `"Your Next Great Deal Awaits!"`, 'font-serif', 'text-primary', 'text-center', 'mt-0.5', 'lg:mt-1', 'font-semibold', 'uppercase', 'md:text-xxl');
+
+//   const formElement = createForm('api', 'login', 'flex', 'flex-col', 'gap-5', 'mt-9', 'mb-16', 'text-primary', 'md:gap-7');
+
+//   const emailGroup = createDiv('flex', 'flex-col', 'gap-1.5');
+//   const labelEmail = createLabel('email', 'Email:', 'md:text-xl');
+//   const emailInput = createInput('text', 'your-email@stud.noroff.no', 'email', 'h-8', 'md:h-10', 'pl-2', 'rounded-xl', 'shadow-customShadow');
+//   const emailValidate = createDiv('text-sm', 'min-h-3.5', 'text-error');
+//   emailValidate.setAttribute('data-validate', 'email');
+//   emailGroup.append(labelEmail, emailInput, emailValidate);
+
+//   const passwordGroup = createDiv('flex', 'flex-col', 'gap-1.5');
+//   const labelPassword = createLabel('password', 'Password:', 'md:text-xl');
+//   const passwordInput = createInput('password', '*****', 'password', 'h-8', 'md:h-10', 'pl-2', 'rounded-xl', 'shadow-customShadow');
+//   const passwordValidate = createDiv('text-sm', 'min-h-3.5', 'text-error');
+//   passwordValidate.setAttribute('data-validate', 'password');
+//   passwordGroup.append(labelPassword, passwordInput, passwordValidate);
+
+//   const ctaGroup = createDiv('flex', 'flex-col', 'mx-auto', 'gap-5');
+//   const wrap = createDiv('mx-auto');
+//   const cta = createInput('submit', '', '', 'uppercase', 'cursor-pointer', 'bg-primary', 'hover:bg-hoverPrimary', 'rounded-xl', 'py-3', 'px-4', 'md:px-6', 'font-serif', 'text-neutralBg', 'shadow-customShadow', 'font-medium', 'md:text-lg');
+//   cta.value = 'login';
+//   wrap.append(cta);
+
+//   const regCta = createAnchor('#', '', '', 'text-center', 'py-2');
+//   const regText1 = createTextNode('Not yet a member? ');
+//   const regText2 = createSpan('Register', 'text-secondary', 'hover:text-hoverSecondary');
+//   const regText3 = createTextNode(' now!');
+//   regCta.append(regText1, regText2, regText3);
+
+//   ctaGroup.append(regCta);
+
+//   const closeBtn = createBtn('', 'absolute', 'top-2.5', 'right-2.5', 'backdrop-invert', 'rounded-full', 'shadow-customShadow', 'hover:animate-pulse');
+//   const closeImg = createImg(closeSvg, 'close', 'size-5');
+//   closeBtn.append(closeImg);
+//   closeBtn.addEventListener('click', () => {
+//     closeModal(modal);
+//   });
+
+//   formElement.append(emailGroup, passwordGroup, wrap);
+//   element.append(imageWrap, heading, formElement, ctaGroup, closeBtn);
+
+//   return element;
+// }
