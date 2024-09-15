8cab882a9faf3ba9275cb1f52b4e17e269a9949d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-15 03:35:23 +0200

    test setup

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,9 @@ import { initializeListings } from './data/initializeListings';
 import { loginBtnListener } from './listeners/loginBtnListener';
 import { renderListings } from './render/renderListings';
 import { renderLoginBtn } from './render/renderLoginBtn';
+import { newProfileModal } from './template/newProfileModal';
 import { ifLoggedIn } from './ui/loggedIn/ifLoggedIn';
+import { openModal } from './ui/modal/openModal';
 import { search } from './ui/search/search';
 
 runApp();
@@ -17,3 +19,10 @@ async function runApp() {
 
   console.log('Listings:', newListings);
 }
+
+// Testing Zone
+// const testingParent = document.querySelector(`[data-modal="modalObject"]`);
+// console.log('Test parent:', testingParent);
+// const testingModal = newProfileModal();
+// openModal(testingModal);
+// End of testing Zone
