ba039312496d920dc9abed2bd2dd15cafbd2b5f9
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-18 19:02:29 +0200

    jsdocs for the new functions

--- src/js/ui/forms/disableElements.js
+++ src/js/ui/forms/disableElements.js
@@ -1,3 +1,15 @@
+/**
+ * Disables HTML elements by their given IDs.
+ *
+ * This function takes one or more element IDs as arguments and disables each corresponding element.
+ * If an element with a provided ID is not found, an error is logged to the console.
+ *
+ * @function disableElements
+ * @param {...string} elementIds - The IDs of the elements to be disabled.
+ * @example
+ * // Disable multiple elements by their IDs
+ * disableElements('submitButton', 'inputField', 'selectDropdown');
+ */
 export function disableElements(...elementIds) {
   elementIds.forEach((elementId) => {
     const element = document.getElementById(elementId);
--- src/js/ui/forms/enableElement.js
+++ src/js/ui/forms/enableElement.js
@@ -1,3 +1,15 @@
+/**
+ * Enables HTML elements by their given IDs if they are disabled.
+ *
+ * This function takes one or more element IDs as arguments and enables each corresponding element if it is currently disabled.
+ * The list of element IDs is cleared after the elements are enabled.
+ *
+ * @function enableElements
+ * @param {...string} elementIds - The IDs of the elements to be enabled.
+ * @example
+ * // Enable multiple elements by their IDs
+ * enableElements('submitButton', 'inputField', 'selectDropdown');
+ */
 export function enableElements(...elementIds) {
   elementIds.forEach((elementId) => {
     const element = document.getElementById(elementId);
