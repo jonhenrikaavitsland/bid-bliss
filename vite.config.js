1c73ec491ffc86a28c5b3e96574593c287131f96
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-18 03:22:58 +0200

    prevents inlining assets

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    assetsInlineLimit: 0, // Set this to 0 to prevent all images from being inlined
+  },
   esbuild: {
     target: 'esnext',
   },
-};
+});
