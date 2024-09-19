5fb6df40645a574cc26d9ee049ed3dbe92f0f220
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-19 20:48:18 +0200

    overlay now has an id and opactiy increased to 60

--- src/js/render/createLoadingOverlay.js
+++ src/js/render/createLoadingOverlay.js
@@ -1,6 +1,7 @@
 import { createDiv } from '../elements/createDiv';
 
 export function createLoadingOverlay() {
-  const overlay = createDiv('absolute', 'bottom-0', 'lef-0', 'w-full', 'h-24', 'md:h-40', 'bg-white', 'bg-opacity-50', 'animate-pulse', 'transition-all', 'duration-500', 'hidden');
+  const overlay = createDiv('absolute', 'bottom-0', 'lef-0', 'w-full', 'h-24', 'md:h-40', 'bg-white', 'bg-opacity-60', 'animate-pulse', 'transition-all', 'duration-500', 'hidden');
+  overlay.setAttribute('id', 'overlay');
   return overlay;
 }
