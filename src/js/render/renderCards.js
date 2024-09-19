dfd94b1335688f99c5eb8d9904c105e8bcbeed9c
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-19 10:29:05 +0200

    updates the number of cards in batch based off screenwidth

--- src/js/render/renderCards.js
+++ src/js/render/renderCards.js
@@ -4,7 +4,19 @@ import { renderNextBatch } from './renderNextBatch';
 
 export function renderCards(listings) {
   const state = { currentIndex: 0 };
-  const batchSize = 15;
+  let batchSize = 15;
+
+  if (window.innerWidth < 1245) {
+    batchSize = batchSize % 2 === 0 ? batchSize : batchSize + 1;
+  }
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 1245) {
+      batchSize = batchSize % 2 === 0 ? batchSize : batchSize + 1;
+    } else {
+      batchSize = 15; // or any other default batch size you want
+    }
+  });
 
   const parent = document.querySelector(`[data-modal="auctionModalDisplayListings"]`);
   const overlay = createLoadingOverlay();
