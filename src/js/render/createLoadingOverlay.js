60521a9f25956cc65126f26487a8ca8ade8a53f9
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-19 10:29:33 +0200

    overlay is taller on wider screens

--- src/js/render/createLoadingOverlay.js
+++ src/js/render/createLoadingOverlay.js
@@ -1,6 +1,6 @@
 import { createDiv } from '../elements/createDiv';
 
 export function createLoadingOverlay() {
-  const overlay = createDiv('absolute', 'bottom-0', 'lef-0', 'w-full', 'h-24', 'bg-white', 'bg-opacity-50', 'animate-pulse', 'transition-all', 'duration-500', 'hidden');
+  const overlay = createDiv('absolute', 'bottom-0', 'lef-0', 'w-full', 'h-24', 'md:h-40', 'bg-white', 'bg-opacity-50', 'animate-pulse', 'transition-all', 'duration-500', 'hidden');
   return overlay;
 }
