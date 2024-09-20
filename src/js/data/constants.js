4cf68bb0fd24c82ef7dcba36c39b21dea3ea86c1
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-09-20 14:56:39 +0200

    updated validation message

--- src/js/data/constants.js
+++ src/js/data/constants.js
@@ -13,7 +13,7 @@ export const API_Key = '469afe69-d51d-4f51-8e40-618b936a4e1a';
 
 export const REG_Email = /^[a-zA-Z0-9._%+-]+@stud\.noroff\.no$/;
 export const REG_Password = /^[a-zA-Z0-9!@#$%^&*()_+={}:;'",.<>?[\]-]+$/;
-export const MSG_Email = 'Invalid email. Please use a @stud.noroff.no email and avoid illegal characters';
+export const MSG_Email = 'Must include @stud.noroff.no';
 export const MSG_Password = 'Invalid password. Must be at least 8 characters long and contain no illegal characters.';
 
 export const DEFAULT_TIME_FORMAT = 'invalid date';
